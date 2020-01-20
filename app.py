93dec07b7f29e1da1ae2ea60e457f23ad6dc986d
yakolan213 <57493477+yakolan213@users.noreply.github.com>
2020-01-20 19:40:53 +0200

    Add files via upload
    added admin in app and option to delete from db

--- app.py
+++ app.py
@@ -12,30 +12,50 @@ class Final_Texts(db.Model):
     num_of_offers = db.Column(db.Integer)
     num_of_accepts = db.Column(db.Integer)
     gadget = db.Column(db.String(10))
-    submit_time = db.Column(db.DateTime, default=datetime.utcnow())
+    submit_time = db.Column(db.DateTime, default=datetime.now())
 
     def __repr__(self):
-        return 'Thanks for Participating'
+        return '<Text %r>' % self.id
 
 @app.route('/', methods=['POST','GET'])
 def index():
     if request.method == 'POST':
         text_content = request.form['text_content']
-        new_text = Final_Texts(content=text_content)
+        new_text = Final_Texts(full_text=text_content)
 
         try:
             db.session.add(new_text)
             db.session.commit()
-            return redirect('/')
+            return 'Thanks For Participating'
         except:
             return 'There was an issue with adding your text'
 
     else:
         return render_template('index.html')
 
-# @app.route('/delete/<int:id>')
-# def delete(id):
-#     text_to_delete = Final_Texts.query.get_or_404(id)
+@app.route('/admin', methods=['POST','GET'])
+def admin_index():
+    if request.method == 'POST':
+        text_content = request.form['text_content']
+        new_text = Final_Texts(full_text=text_content)
+    else:
+        texts = Final_Texts.query.order_by(Final_Texts.id).all()
+        return render_template('admin.html',texts=texts)
+
+def get_text(id):
+    if request.method == 'POST':
+        text = Final_Texts.query.filter_by(id=id).first()
+        return f"Text for id {Final_Texts.id} is: {text.full_text}"
+
+@app.route('/admin/delete/<int:id>')
+def delete(id):
+     text_to_delete = Final_Texts.query.get_or_404(id)
+     try:
+         db.session.delete(text_to_delete)
+         db.session.commit()
+         return redirect('/admin')
+     except:
+         return 'There seems to be a problem deleting'
 
 if __name__ == "__main__":
     app.run(debug=True)
