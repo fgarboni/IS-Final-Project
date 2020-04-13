6289b9e645cc189245195c03264e34f30f08c348
yakolan213 <57493477+yakolan213@users.noreply.github.com>
2020-04-13 10:03:35 +0300

    update for returning variable to ajax

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
 from flask import Flask, render_template, url_for, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import tensorflow as tf
-from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
+# import tensorflow as tf
+# from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
 
 
-def activate_model():
-    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-    # add the EOS token as PAD token to avoid warnings
-    model = TFGPT2LMHeadModel.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id)
-    return tokenizer, model
-    # encode context the generation is conditioned on
+# def activate_model():
+#     tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+#     # add the EOS token as PAD token to avoid warnings
+#     model = TFGPT2LMHeadModel.from_pretrained("gpt2", pad_token_id=tokenizer.eos_token_id)
+#     return tokenizer, model
+#     # encode context the generation is conditioned on
+#
+#
+# def predict(words, num_pred, tokenizer, model):
+#     word_amount = len(words.split())
+#     predicted_words = word_amount+num_pred
+#     input_ids = tokenizer.encode(words, return_tensors='tf')
+#     # generate text until the output length (which includes the context length) reaches 50
+#     greedy_output = model.generate(input_ids, max_length=predicted_words)
+#     complete_sentence = tokenizer.decode(greedy_output[0], skip_special_tokens=True)
+#     predicted_part = complete_sentence.replace(words, '')
+#     return predicted_part
 
 
-def predict(words, num_pred, tokenizer, model):
-    word_amount = len(words.split())
-    predicted_words = word_amount+num_pred
-    input_ids = tokenizer.encode(words, return_tensors='tf')
-    # generate text until the output length (which includes the context length) reaches 50
-    greedy_output = model.generate(input_ids, max_length=predicted_words)
-    complete_sentence = tokenizer.decode(greedy_output[0], skip_special_tokens=True)
-    predicted_part = complete_sentence.replace(words, '')
-    return predicted_part
-
-
-token, model_test = activate_model()
+# token, model_test = activate_model()
 
 
 app = Flask(__name__)
@@ -63,9 +63,12 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def process():
+    rf = request.form
+    print(rf)
     text = request.form['name']
     if text:
-        new_text = predict(text, 1, token, model_test)
+        # new_text = predict(text, 1, token, model_test)
+        new_text = text[::-1]
         return jsonify({'name': new_text})
 
     return jsonify({'error': 'Missing data!'})
